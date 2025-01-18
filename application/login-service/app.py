ad0c2d2d9c241b693eb9e623ef271dd03a3d3ad4
apipinic <98458664+apipinic@users.noreply.github.com>
2025-01-18 20:40:29 +0100

    Update app.py

--- application/login-service/app.py
+++ application/login-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request  # Add 'request'
 from flask_login import LoginManager, login_user, logout_user, current_user, UserMixin, login_required
 from flask_jwt_extended import JWTManager, create_access_token
 from oauthlib.oauth2 import WebApplicationClient
