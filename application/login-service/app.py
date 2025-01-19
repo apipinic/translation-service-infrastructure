092c0863b0f4f17c7cf9a9cc7f848d7519877e27
apipinic <98458664+apipinic@users.noreply.github.com>
2025-01-19 21:02:55 +0100

    Update

--- application/login-service/app.py
+++ application/login-service/app.py
@@ -129,7 +129,7 @@ def callback():
                 "token",
                 token,
                 secure=True,
-                httponly=True,
+                httponly=False,
                 samesite="Lax",
             )
             return response
